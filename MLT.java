afc34be61ec14026819068c8ece9ff735a517740
amaniosman362-debug <amaniosman362@gmail.com>
2025-09-29 13:31:20 +0300

    Add files via upload
    Learning Math tools using Java.

---
+++ MLT.java
@@ -0,0 +1,21 @@
+
+package vu.mlt;
+import java.util.Scanner;
+
+
+public class MLT {
+
+    public static void main(String[] args) {
+      Scanner Save =new Scanner(System.in);        
+        System.out.println("What is \"7+9 \" ?");
+        int option =Save.nextInt();
+        int answer =16;
+        if(option==answer){
+            System.out.println("True");
+        }
+    
+        else
+        
+            System.out.println("False");
+}
+}
